5421aa2adeec26db63a7b614c89edcfd4756a380
Juliano Seus <julianoseusdev@outlook.com>
2022-01-31 20:07:54 -0300

    Connecting Angular with List Employee REST API

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 
@@ -10,7 +12,8 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
     EmployeeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
