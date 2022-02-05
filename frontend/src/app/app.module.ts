d2e5204fe171f4bac955cd3e8f2f0fc5b42c6bab
Juliano Seus <julianoseusdev@outlook.com>
2022-02-05 00:03:25 -0300

    Angular Create Employee Form Handling

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { CreateEmployeeComponent } from './create-employee/create-employee.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
